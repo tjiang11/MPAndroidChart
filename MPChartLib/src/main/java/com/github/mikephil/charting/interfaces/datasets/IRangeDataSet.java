711c471a190472392ea8fc4b2c44ea65def25535
Tony <tjiang@alarm.com>
2017-06-19 10:51:58 -0400

    Create IRangeDataSet

---
+++ MPChartLib/src/main/java/com/github/mikephil/charting/interfaces/datasets/IRangeDataSet.java
@@ -0,0 +1,34 @@
+package com.github.mikephil.charting.interfaces.datasets;
+
+import com.github.mikephil.charting.data.RangeEntry;
+
+/**
+ * Created by TJiang on 6/19/2017.
+ */
+
+public interface IRangeDataSet extends IBarLineScatterCandleBubbleDataSet<RangeEntry> {
+    /**
+     * Returns the width used for drawing borders around the ranges.
+     * If borderWidth == 0, no border will be drawn.
+     *
+     * @return
+     */
+    float getBarBorderWidth();
+
+    /**
+     * Returns the color drawing borders around the ranges.
+     *
+     * @return
+     */
+    int getBarBorderColor();
+
+    /**
+     * Returns the alpha value (transparency) that is used for drawing the
+     * highlight indicator.
+     *
+     * @return
+     */
+    int getHighLightAlpha();
+
+
+}
